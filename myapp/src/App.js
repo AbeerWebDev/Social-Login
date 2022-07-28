be4f9d4b78346a199e55078b89af30b8a1962b11
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-28 19:50:10 +0400

    Navbar done and data added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import './app.css'
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
         <span className='logo'>Abeer's App</span>
         <ul className="list">
             <li className="listItem">
-                <img src="" alt="" className="avatar" />
+                <img 
+                src="https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                alt="" 
+                className="avatar" 
+                />
             </li>
             <li className="listItem">John Doe</li>
             <li className="listItem">Logout</li>
