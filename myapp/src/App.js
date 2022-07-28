2f23306de404a1261d2fbe13739319355836436d
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-28 14:13:17 +0400

    Navbar in progress

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <span className='logo'>Abeer's App</span>
+        <ul className="list">
+            <li className="listItem">
+                <img src="" alt="" className="avatar" />
+            </li>
+            <li className="listItem">John Doe</li>
+            <li className="listItem">Logout</li>
+        </ul>
+    
+    </div>
+  )
+}
+
+export default Navbar
