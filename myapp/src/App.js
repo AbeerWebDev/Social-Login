6f8fd66320dfcda23e508f4cf76ef532c17a6575
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-07-30 13:11:18 +0400

    Changed user to false

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user = true;
+  const user = false;
 
   return (
     <BrowserRouter>
